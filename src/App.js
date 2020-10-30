dc28f0a85b0c86f6a4d42a9ac93bf0750b06e9c3
Renan <renanmendes@pbhub.com.br>
2020-10-29 22:55:36 -0300

    advertisings sections: digital account, credit card, rewards and relation money people

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import logo from './img/logo.svg';
 import bannerImg from './img/banner.png';
 import fgtsImg from './img/fgts.png';
+import phone from './img/phone.png';
+import creditCard from './img/creditCard.png';
+import rewards from './img/rewards.png';
 
 import { 
   Container,
@@ -15,10 +18,20 @@ import {
   LoginButton,
   LoginIcon,
   SignUp,
+  ArrowIcon,
   Details,
   BannerButton,
   BringYouHelp,
-  BringYouHelpLeft
+  BringYouHelpLeft,
+  DigitalAccount,
+  DigitalAccountLeft,
+  CreditCard,
+  CreditCardLeft,
+  Bold,
+  BoldRewards,
+  Rewards,
+  RewardsLeft,
+  RelationMoneyPeople
 } from './styles';
 
 function App() {
@@ -52,7 +65,8 @@ function App() {
         <Banner>
           <Details>
             <h1>Pix: um novo <br/> jeito de fazer <br/> transferências e <br/> pagamentos</h1>
-            <span>Saiba como funciona</span>
+            <span>Saiba como funciona <ArrowIcon/> </span>
+            
             <BannerButton>Quero ser nubank</BannerButton>
           </Details>
           <img src={bannerImg} alt="banner_image"/>
@@ -60,13 +74,54 @@ function App() {
       </Header>
 
       <main>
+
         <BringYouHelp>
           <BringYouHelpLeft>
             <h1>Traga seu <br/> FGTS e Auxílio <br/> Emergencial <br/> para o Nubank</h1>
-            <span>Confira os benefícios </span>
+            <span>Confira os benefícios <ArrowIcon/> </span> 
           </BringYouHelpLeft>
-          <img src={fgtsImg} alt="banner_image"/>
+          <img src={fgtsImg} alt="ftgs_image"/>
         </BringYouHelp>
+
+        <DigitalAccount>
+          <DigitalAccountLeft>
+            <h1>
+              <Bold> <a href="#">Conta do Nubank</a> </Bold> 
+              Nossa conta digital <br/> com mais de 20 <br/> milhões de clientes <br/> em todo o Brasil.
+            </h1>
+            <span>Segura e sem complicações <ArrowIcon/> </span> 
+          </DigitalAccountLeft>
+          <img src={phone} alt="phone_image"/>
+        </DigitalAccount>
+
+        <CreditCard>
+          <CreditCardLeft>
+            <h1>
+              <Bold> <a href="#">Cartão de Crédito</a> </Bold> 
+              Pode chamar ele de <br/> roxinho. Além disso, <br/> pode chamar ele de <br/> moderno, gratuito e <br/> prático também.
+            </h1>
+            <span>Saiba mais <ArrowIcon/> </span> 
+          </CreditCardLeft>
+          <img src={creditCard} alt="creditCard_image"/>
+        </CreditCard>
+
+        <Rewards>
+          <RewardsLeft>
+            <h1>
+              <BoldRewards> <a href="#">Rewards</a> </BoldRewards> 
+              Um programa de <br/> pontos justo e fácil <br/> de usar.
+            </h1>
+            <span>Saiba mais <ArrowIcon/> </span> 
+          </RewardsLeft>
+          <img src={rewards} alt="rewards_image"/>
+        </Rewards>
+
+        <RelationMoneyPeople>
+            <h1>
+              Reinventando <br/> a relação das <br/> pessoas com <br/> o dinheiro
+            </h1>
+            <span>É nisso que acreditamos <ArrowIcon/> </span> 
+        </RelationMoneyPeople>
 
       </main>
     </Container>
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import {CgLogIn} from 'react-icons/cg';
+import { AiOutlineArrowRight } from "react-icons/ai";
+import relationMoneyPeople from './img/relationMoneyPeople.png';
 
 export const Container = styled.div`
   
@@ -11,6 +13,7 @@ export const Header = styled.div`
 `
 
 export const Menu = styled.div`
+    /* position: fixed; */
     display: flex;
     align-items:center;
     justify-content: space-between;   
@@ -110,8 +113,16 @@ export const Banner = styled.div`
         letter-spacing: -0.03em;
         font-size: 1.125rem;
         margin-top: 15px;
+        display: flex;
+        align-items: center;
+
+        svg {
+            margin-top: 1px;
+            margin-left: 5px;
+        }
     }
 `
+export const ArrowIcon = styled(AiOutlineArrowRight)``;
 
 export const Details = styled.div`
     display: flex;
@@ -164,9 +175,200 @@ export const BringYouHelpLeft = styled.div`
         letter-spacing: -0.03em;
         font-size: 1.125rem;
         font-weight: 500;
-        color: #9359c1;
-        /* margin-top: 30px; */
+        color: #9359c1;        
+        display: flex;
+        align-items: center;
+
+        svg {
+            margin-top: 1px;
+            margin-left: 5px;
+        }
+    }
+`;
+
+export const DigitalAccount = styled.section`
+    padding: 48px 0px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #f5f5f5;
+
+    img {
+        max-width: 50%;
+        /* height: 375px; */
+    }
+`;
+
+export const DigitalAccountLeft = styled.div`
+    max-width: 100%;
+    padding: 20px;
+    h1 {
+        font-size: 3rem;
+        font-weight: 500;
+        letter-spacing: -0.03em;
+        line-height: 1.16;
+        margin-bottom: 15px;
+    }
+    
+
+    span {
+        cursor: pointer;
+        line-height: 1.35;
+        letter-spacing: -0.03em;
+        font-size: 1.125rem;
+        font-weight: 500;
+        color: #9359c1;        
+        display: flex;
+        align-items: center;
+
+        svg {
+            margin-top: 1px;
+            margin-left: 5px;
+        }
+    }
+`;
+
+export const Bold = styled.p`
+   a { 
+        color: #8A05BE;
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+        }
+   }
+`;
+
+export const CreditCard = styled.section`
+    padding: 48px 0px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    img {
+        max-width: 18%;
     }
 
+`;
+export const CreditCardLeft = styled.div`
+    max-width: 100%;
+    padding: 20px;
+    h1 {
+        font-size: 3rem;
+        font-weight: 500;
+        letter-spacing: -0.03em;
+        line-height: 1.16;
+        margin-bottom: 15px;
+    }
+    
 
+    span {
+        cursor: pointer;
+        line-height: 1.35;
+        letter-spacing: -0.03em;
+        font-size: 1.125rem;
+        font-weight: 500;
+        color: #9359c1;        
+        display: flex;
+        align-items: center;
+
+        svg {
+            margin-top: 1px;
+            margin-left: 5px;
+        }
+    }
+`;
+
+export const BoldRewards = styled.p`
+   a { 
+        color: #C093FB;
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+        }
+   }
+`;
+
+export const Rewards = styled.section`
+    padding: 48px 0px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #530082;
+
+    img {
+        max-width: 30%;
+    }
+`;
+
+export const RewardsLeft = styled.div`
+    max-width: 100%;
+    padding: 20px;
+    color: #fff;
+    h1 {
+        font-size: 3rem;
+        font-weight: 500;
+        letter-spacing: -0.03em;
+        line-height: 1.16;
+        margin-bottom: 15px;
+    }
+    
+
+    span {
+        cursor: pointer;
+        line-height: 1.35;
+        letter-spacing: -0.03em;
+        font-size: 1.125rem;
+        font-weight: 500;
+        color: #fff;        
+        display: flex;
+        align-items: center;
+
+        svg {
+            margin-top: 1px;
+            margin-left: 5px;
+        }
+    }
+
+`;
+
+export const RelationMoneyPeople = styled.article `
+    max-width: 100%;
+    height: 600px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    background-size: cover;
+    background-position: center center;
+    background-color: rgb(101, 120, 136);
+    background-image: url(${relationMoneyPeople});
+
+    h1 {
+        font-size: 3rem;
+        font-weight: 500;
+        letter-spacing: -0.03em;
+        line-height: 1.16;
+        margin-left: 60px;
+        margin-bottom: 15px;
+        color: #fff;     
+    }
+    
+
+    span {
+        cursor: pointer;
+        line-height: 1.35;
+        letter-spacing: -0.03em;
+        font-size: 1.125rem;
+        font-weight: 500;
+        color: #fff;        
+        display: flex;
+        align-items: center;
+        margin-left: 60px;
+
+        svg {
+            margin-top: 1px;
+            margin-left: 5px;
+        }
+    }
 `;
