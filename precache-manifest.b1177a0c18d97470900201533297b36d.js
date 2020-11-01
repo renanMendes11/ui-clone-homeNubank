28e2f4d23fac22da1e82527e5c00c721c65e638a
Renan <renanmendes@pbhub.com.br>
2020-11-01 03:05:44 -0300

    Updates

---
+++ precache-manifest.b1177a0c18d97470900201533297b36d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6df20f78dd56b2a73ddcf4ce0d259c1",
+    "url": "/ui-clone-homeNubank/index.html"
+  },
+  {
+    "revision": "d7ee6e023335cf33adf9",
+    "url": "/ui-clone-homeNubank/static/css/main.a7bd4dea.chunk.css"
+  },
+  {
+    "revision": "665c7e30dbc226a934b8",
+    "url": "/ui-clone-homeNubank/static/js/2.98864c4b.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/ui-clone-homeNubank/static/js/2.98864c4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ee6e023335cf33adf9",
+    "url": "/ui-clone-homeNubank/static/js/main.a5b5e56c.chunk.js"
+  },
+  {
+    "revision": "6bd9b7af241410a3f499",
+    "url": "/ui-clone-homeNubank/static/js/runtime-main.7e8af8ec.js"
+  },
+  {
+    "revision": "e15fdae704986353baa4578b5a50149a",
+    "url": "/ui-clone-homeNubank/static/media/banner.e15fdae7.png"
+  },
+  {
+    "revision": "b1d32368f7c53c105ccff2d3d14d5b7a",
+    "url": "/ui-clone-homeNubank/static/media/blogImage.b1d32368.png"
+  },
+  {
+    "revision": "82fb8a48a1c48e1af52bea8bf8e799b9",
+    "url": "/ui-clone-homeNubank/static/media/creditCard.82fb8a48.png"
+  },
+  {
+    "revision": "5478326bcabb5ebc44e110bb75bf63ed",
+    "url": "/ui-clone-homeNubank/static/media/fgts.5478326b.png"
+  },
+  {
+    "revision": "d00790933976d39853e8b3cbc7b2f746",
+    "url": "/ui-clone-homeNubank/static/media/logo.d0079093.svg"
+  },
+  {
+    "revision": "a62f6c940498e28242d4ad84f4730942",
+    "url": "/ui-clone-homeNubank/static/media/nuCommunity.a62f6c94.png"
+  },
+  {
+    "revision": "10a21ac659550fdd6c2941cd7ec275ab",
+    "url": "/ui-clone-homeNubank/static/media/phone.10a21ac6.png"
+  },
+  {
+    "revision": "2af80081db969ec5f038fea9b4547563",
+    "url": "/ui-clone-homeNubank/static/media/relationMoneyPeople.2af80081.png"
+  },
+  {
+    "revision": "b0b781c163dae4ea5281173ea33043f8",
+    "url": "/ui-clone-homeNubank/static/media/rewards.b0b781c1.png"
+  }
+]);
